ca7f0123ce488346e97cca583ba5fd2dc8bfedce
Xymena Vazquez <xymenavaz@gmail.com>
2024-10-28 15:17:17 -0600

    Add files via upload

---
+++ ciclos for funciones.R
@@ -0,0 +1,61 @@
+#actividad con ciclos for y fnciones
+sample(2: 34, 10)
+
+ex1<-excusas2024 [sample(1:33, 1), 1]
+ex2<-excusas2024 [sample(1:33, 1), 2]
+ex3<-excusas2024 [sample(1:33, 1), 3]
+
+
+
+excusa<-function(){
+  ex1<-excusas2024 [sample(1:33, 1), 1]
+  ex2<-excusas2024 [sample(1:33, 1), 2]
+  ex3<-excusas2024 [sample(1:33, 1), 3]
+  for (i in 1:10) {
+    return(paste(ex1, ex2, ex3))
+  }
+}
+
+
+
+}
+
+ex1<-excusas2024 [sample(1:33, 1), 1]
+ex2<-excusas2024 [sample(1:33, 1), 2]
+ex3<-excusas2024 [sample(1:33, 1), 3]
+
+excusa<-function(a, b, c){
+  ex1<-excusas2024 [sample(1:33, 1), 1]
+  ex2<-excusas2024 [sample(1:33, 1), 2]
+  ex3<-excusas2024 [sample(1:33, 1), 3]
+  for (i in 1:10) {
+  }
+    return(paste(a, b, c))
+  
+}
+
+excusa(ex1, ex2, ex3)
+
+excusas<-function(){
+for (i in 1:10) {
+ excusa<-read.csv("")
+ex1<-excusas2024 [sample(1:33, 1), 1]
+ex2<-excusas2024 [sample(1:33, 1), 2]
+ex3<-excusas2024 [sample(1:33, 1), 3]
+print(paste(ex1, ex2, ex3))
+}
+}
+excusas
+
+
+
+#hacer una tabla de multiplicación
+
+multiplicaciones<-function(a){
+  for(c in 1:10){
+multiplicaciones<-a * c
+    print(multiplicaciones)
+}
+}
+multiplicaciones(3)
+
